acbefaf41a1d6455d8362a10e16c0e04061e05db
ait <ait@ex.com>
2019-06-30 08:11:15 +0900

    start

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+{
+  // const ul = document.querySelector('ul');
+  const ul = document.querySelector('ul');
+  console.log(ul.parentNode);
+  console.log(ul.children[3]);
+  for (let i = 0; i < ul.children.length; i++) {
+    console.log(ul.children[i].textContent);
+  }
+
+//
+  const h1 = document.querySelector('h1');
+  console.log(h1.title);
+  h1.title = 'Change!';
+
+  h1.style.color = "orange";
+}
